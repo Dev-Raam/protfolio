be9d01f9f17df2a18c9e7daf840d081634bd741f
Dev-Raam <raamgithub63@gmail.com>
2025-12-08 21:45:32 +0530

    Modified

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,9 @@ import { GeistMono } from "geist/font/mono"
 import "./globals.css"
 
 export const metadata: Metadata = {
-  title: "v0 App",
-  description: "Created with v0",
-  generator: "v0.app",
+  title: "Protfolio",
+  description: "My personal portfolio website.",
+  generator: "Next.js",
 }
 
 export default function RootLayout({
